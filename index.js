b5a106eeb3889d7cc3165a6c73d6c8656d1562af
Supriyo Saha <mm.saha1982@gmail.com>
2023-02-18 00:25:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const track = document.getElementById("image-track");
+window.onmousedown = e =>{
+track.dataset.mouseDownaAt = e.clientX;
+}
+window.onmouseup = () =>{
+    track.dataset.mouseDownaAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove= e =>{
+    if(track.dataset.mouseDownaAt === "0")return;
+   const mouseDelta = parseFloat(track.dataset.mouseDownaAt) - e.clientX;
+   maxDelta = window.innerWidth /2;
+
+  const percentage = (mouseDelta/maxDelta)* -100;
+  const nextPercentageUncontrained = parseFloat(track.dataset.prevPercentage) + percentage;
+ const nextPercentage=Math.max(Math.min(nextPercentageUncontrained,0),-100);
+
+   
+ track.dataset.percentage = nextPercentage;
+  track.animate({
+    transform : `translate(${nextPercentage}%, -50%)`
+  }, {duration : 1200, fill: "forwards"});
+
+for (const image of track.getElementsByClassName("image")){
+    image.animate({
+        objectPosition: `${100+ nextPercentage}% center`},{duration:1200, fill: "forwards"}
+    );
+}
+}
